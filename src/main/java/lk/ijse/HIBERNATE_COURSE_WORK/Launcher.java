525ba33e99b970a8f96d46237b0d721a71887dd5
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-02-29 21:57:35 +0530

    project structure done

---
+++ src/main/java/lk/ijse/HIBERNATE_COURSE_WORK/Launcher.java
@@ -0,0 +1,19 @@
+package lk.ijse.HIBERNATE_COURSE_WORK;
+/* 
+    @author Sachi_S_Bandara
+    @created 2/29/2024 - 9:54 PM 
+*/
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher  extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
---
+++ src/main/java/lk/ijse/HIBERNATE_COURSE_WORK/LauncherWrapper.java
@@ -0,0 +1,11 @@
+package lk.ijse.HIBERNATE_COURSE_WORK;
+/* 
+    @author Sachi_S_Bandara
+    @created 2/29/2024 - 9:54 PM 
+*/
+
+public class LauncherWrapper {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
