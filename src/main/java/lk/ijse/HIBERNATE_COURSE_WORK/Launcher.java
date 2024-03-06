e258a3f8b3bd7a9bd8f7936292a848ec685a79f7
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-03-06 21:25:56 +0530

    content update

--- src/main/java/lk/ijse/HIBERNATE_COURSE_WORK/Launcher.java
+++ src/main/java/lk/ijse/HIBERNATE_COURSE_WORK/Launcher.java
@@ -5,7 +5,12 @@ package lk.ijse.HIBERNATE_COURSE_WORK;
 */
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Launcher  extends Application {
     public static void main(String[] args) {
@@ -14,6 +19,14 @@ public class Launcher  extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Launcher.class.getResource("/view/loginPage.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+
+
+        stage.setTitle("");
+        stage.show();
 
     }
 }
