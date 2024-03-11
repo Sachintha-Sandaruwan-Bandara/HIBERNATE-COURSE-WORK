034185fc0d08180a5ce72321dc828eddd9e25b3c
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-03-11 11:15:59 +0530

    LibraryBranchDTO toEntity method done

--- src/main/java/lk/ijse/HIBERNATE_COURSE_WORK/dto/LibraryBranchDTO.java
+++ src/main/java/lk/ijse/HIBERNATE_COURSE_WORK/dto/LibraryBranchDTO.java
@@ -5,14 +5,16 @@ package lk.ijse.HIBERNATE_COURSE_WORK.dto;
 */
 
 
+import lk.ijse.HIBERNATE_COURSE_WORK.entity.Book;
 import lk.ijse.HIBERNATE_COURSE_WORK.entity.LibraryBranch;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.ManyToOne;
-
+import java.util.ArrayList;
 import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -21,17 +23,24 @@ public class LibraryBranchDTO {
 
     private String name;
     private String location;
-    private List<BookDTO> books;
 
     @ManyToOne
     private AdminDTO admin;
+    private List<BookDTO> books;
 
-    public LibraryBranch toEntity(){
+
+    public LibraryBranch toEntity() {
         LibraryBranch libraryBranch = new LibraryBranch();
         libraryBranch.setId(this.id);
         libraryBranch.setName(this.name);
         libraryBranch.setLocation(this.location);
-       return libraryBranch;
+        libraryBranch.setAdmin(this.admin.toEntity());
+        List<Book> bookList = new ArrayList<>();
+        for (BookDTO bookDto : this.books) {
+            bookList.add(bookDto.toEntity());
+        }
+        libraryBranch.setBooks(bookList);
+        return libraryBranch;
     }
 
 }
