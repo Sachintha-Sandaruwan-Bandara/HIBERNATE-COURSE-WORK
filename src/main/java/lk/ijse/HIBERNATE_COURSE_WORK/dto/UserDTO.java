31dbffb2e65642f37317af848de5d775c94174a6
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-03-11 11:21:32 +0530

    UserDTO toEntity method done

--- src/main/java/lk/ijse/HIBERNATE_COURSE_WORK/dto/UserDTO.java
+++ src/main/java/lk/ijse/HIBERNATE_COURSE_WORK/dto/UserDTO.java
@@ -4,11 +4,13 @@ package lk.ijse.HIBERNATE_COURSE_WORK.dto;
     @created 3/7/2024 - 8:30 PM 
 */
 
+import lk.ijse.HIBERNATE_COURSE_WORK.entity.Transaction;
 import lk.ijse.HIBERNATE_COURSE_WORK.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,14 +20,22 @@ public class UserDTO {
     private String username;
     private String password;
     private String email;
-    private List<TransactionDTO> transactions;
+
     private AdminDTO admin;
+    private List<TransactionDTO> transactions;
+
 public User toEntity(){
     User user = new User();
     user.setId(this.id);
     user.setUsername(this.username);
     user.setPassword(this.password);
     user.setEmail(this.email);
+    user.setAdmin(this.admin.toEntity());
+    List<Transaction>transactionList=new ArrayList<>();
+    for (TransactionDTO transactionDto:transactions) {
+        transactionList.add(transactionDto.toEntity());
+    }
+    user.setTransactions(transactionList);
     return user;
 }
 
