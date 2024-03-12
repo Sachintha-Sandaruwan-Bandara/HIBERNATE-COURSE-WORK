5d8cbfc1e52c63fe5df4ebde2b15f99b3d6960eb
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-03-12 15:24:35 +0530

    transaction crud done

--- src/main/java/lk/ijse/HIBERNATE_COURSE_WORK/service/impl/TransactionServiceImpl.java
+++ src/main/java/lk/ijse/HIBERNATE_COURSE_WORK/service/impl/TransactionServiceImpl.java
@@ -8,8 +8,11 @@ import lk.ijse.HIBERNATE_COURSE_WORK.dto.TransactionDTO;
 import lk.ijse.HIBERNATE_COURSE_WORK.repository.TransactionRepository;
 import lk.ijse.HIBERNATE_COURSE_WORK.repository.impl.TransactionRepositoryImpl;
 import lk.ijse.HIBERNATE_COURSE_WORK.service.TransactionService;
+import lk.ijse.HIBERNATE_COURSE_WORK.util.SessionFactoryConfig;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionServiceImpl implements TransactionService {
@@ -28,26 +31,86 @@ public class TransactionServiceImpl implements TransactionService {
     }
     @Override
     public Long saveTransaction(TransactionDTO transactionDTO) {
-        return null;
+        session = SessionFactoryConfig.getInstance()
+                .getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            transactionRepository.setSession(session);
+            Long id = transactionRepository.save(transactionDTO.toEntity());
+            transaction.commit();
+            session.close();
+            return id;
+        } catch (Exception ex) {
+            transaction.rollback();
+            session.close();
+            ex.printStackTrace();
+            return -1L;
+        }
     }
 
     @Override
     public TransactionDTO getTransaction(long id) {
-        return null;
+        try {
+            session = SessionFactoryConfig.getInstance()
+                    .getSession();
+            transactionRepository.setSession(session);
+            lk.ijse.HIBERNATE_COURSE_WORK.entity.Transaction transaction = transactionRepository.get(id);
+            session.close();
+            return transaction.toDTO();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
     }
 
     @Override
     public boolean updateTransaction(TransactionDTO transactionDTO) {
-        return false;
+        session = SessionFactoryConfig.getInstance()
+                .getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            transactionRepository.setSession(session);
+           transactionRepository.update(transactionDTO.toEntity());
+            session.close();
+            return true;
+        } catch (Exception ex) {
+            transaction.rollback();
+            session.close();
+            ex.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean deleteTransaction(TransactionDTO transactionDTO) {
-        return false;
+        session = SessionFactoryConfig.getInstance()
+                .getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            transactionRepository.setSession(session);
+            transactionRepository.delete(transactionDTO.toEntity());
+            transaction.commit();
+            session.close();
+            return true;
+        } catch (Exception ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+            session.close();
+            return false;
+        }
     }
 
     @Override
     public List<TransactionDTO> getAllTransactions() {
-        return null;
+        session = SessionFactoryConfig.getInstance()
+                .getSession();
+        transactionRepository.setSession(session);
+        List<lk.ijse.HIBERNATE_COURSE_WORK.entity.Transaction> allTransactions= transactionRepository.getAll();
+        List<TransactionDTO> transactionDTOList = new ArrayList<>();
+        for (lk.ijse.HIBERNATE_COURSE_WORK.entity.Transaction transaction: allTransactions) {
+            transactionDTOList.add(transaction.toDTO());
+        }
+        return transactionDTOList;
     }
+
 }
